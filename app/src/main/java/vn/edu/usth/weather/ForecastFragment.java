286b0e90457d241afa387df7602fe1581f40c59c
huyennvuu <vukhanhhuyen99@gmail.com>
2019-11-15 17:04:26 +0700

    Practical Work 5

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -23,22 +23,35 @@ public class ForecastFragment extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle saveInstanceState )
     {
-        View view = inflater.inflate(R.layout.weather_activity, container, false);
-        LinearLayout ll = view.findViewById(R.id.container);
-        ll.setGravity(Gravity.CENTER);
+        View view = inflater.inflate(R.layout.forecast_fragment, container, false);
+//        LinearLayout ll = view.findViewById(R.id.container);
+//        ll.setGravity(Gravity.CENTER);
+//
+//        TextView mon_text = new TextView(getContext());
+//        TextView tues_text = new TextView(getContext());
+//        TextView wed_text = new TextView(getContext());
 
-        TextView tv = new TextView(getContext());
-        tv.setText("Today");
-        tv.setPadding(10,10,10,50);
-        tv.setTextSize(30);
-        tv.setGravity(Gravity.CENTER);
-        tv.setTypeface(Typeface.DEFAULT_BOLD);
-        ll.addView(tv);
+//        tv.setText("Yesterday");
+//        tv.setPadding(10,10,10,50);
+//        tv.setTextSize(30);
+//        tv.setTypeface(Typeface.DEFAULT_BOLD);
+//        ll.addView(tv);
 
-        ImageView iv = new ImageView(getContext());
-        iv.setImageResource(R.drawable.sunny);
-        ll.addView(iv);
+//        tv.setText("Today");
+//        tv.setPadding(10,10,10,50);
+//        tv.setTextSize(30);
+//        tv.setTypeface(Typeface.DEFAULT_BOLD);
+//        ll.addView(tv);
 
+//        tv.setText("Tomorow");
+//        tv.setPadding(10,10,10,50);
+//        tv.setTextSize(30);
+//        tv.setTypeface(Typeface.DEFAULT_BOLD);
+//        ll.addView(tv);
+
+//        ImageView iv = new ImageView(getContext());
+//        iv.setImageResource(R.drawable.sunny);
+//        ll.addView(iv);
         view.setBackgroundColor(Color.rgb(232,178,178));
         return view;
     }
