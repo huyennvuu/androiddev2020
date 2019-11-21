26310bbeb65ee24b1c1df423b75f7395b07ae990
huyennvuu <vukhanhhuyen99@gmail.com>
2019-11-22 03:02:35 +0700

    Practical Work 6

--- app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -1,28 +1,22 @@
 package vn.edu.usth.weather;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ForecastFragment extends Fragment
+public class WeatherFragment extends Fragment
 {
-    public ForecastFragment()
+    public WeatherFragment()
     {
     }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle saveInstanceState )
     {
-        return inflater.inflate(R.layout.forecast_fragment, container, false);
+        return inflater.inflate(R.layout.wearther_fragment, container, false);
     }
 }
