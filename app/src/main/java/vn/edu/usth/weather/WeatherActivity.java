ce0b55f46ff3844a8c961b0848a48507046ff2dd
huyennvuu <vukhanhhuyen99@gmail.com>
2019-11-22 12:56:48 +0700

    Practical Work 6

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class WeatherActivity extends FragmentActivity {
 
@@ -20,35 +22,12 @@ public class WeatherActivity extends FragmentActivity {
         // Create a new Fragment
         ForecastFragment firstFragment = new ForecastFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG,"This is onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG,"This is onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG,"This is onDestroy");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG,"This is onPause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG,"This is onResume");
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setPageMargin(15);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
     }
 }
